a90fc4f37b79b8a69ae5d271cf6c08335896a0cd
Victor Goff <keeperotphones@gmail.com>
2015-04-19 19:04:24 -0400

    Closed the files... oops
    Also cleaned up some whitespace issues

--- 2_adventures/001/Dino-Herbert/07_Apr19/reg_exp_test3.rb
+++ 2_adventures/001/Dino-Herbert/07_Apr19/reg_exp_test3.rb
@@ -7,22 +7,18 @@ local_file = "MLBscores" + Date.today.to_s + ".txt"
 puts "\nReading from web..."
 
 reg_exp_pat = /Baseball scoreboard for \w+, \w+ \d{2}, \d{4}/
-reg_exp_pat2 = /[A-Z]+ .[0-9]+. @ [A-Z]+ .[0-9]+./ 
-  
+reg_exp_pat2 = /[A-Z]+ .[0-9]+. @ [A-Z]+ .[0-9]+./
+
 file = File.open(local_file, "w")
 file2 = File.open(local_file, "a")
-  
-open(url).each do |line| 
-  a = line.scan(reg_exp_pat) 
+
+open(url).each do |line|
+  a = line.scan(reg_exp_pat)
   b = line.scan(reg_exp_pat2)
   file.write(a)
   file2.write(b)
-      
+
 end
 
-
- 
- 
- 
-
-
+file.close
+file2.close
